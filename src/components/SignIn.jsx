fba1f8dbb442cfe13a6ff45959a07b98d012c357
Your Namefotostoys <ciel2000@gmail.com>
2022-08-16 17:03:10 -0300

    signIn

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import GoogleButton from 'react-google-button'
+import React from "react";
+//import GoogleButton from 'react-google-button'
 
-import {auth} from '../firebase'
-import {GoogleAuthProvider, signInWithRedirect} from 'firebase/auth'
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 const style = {
-    centrar: `flex justify-center w-full h-full`
-}
+  centrar: `flex justify-center w-full h-full`,
+  button: `bg-[#212121] px-4 py-0`,
+  h5: `text-white text-[12px]`,
+};
 
 const googleSignIn = () => {
-    const provider = new GoogleAuthProvider()
-    signInWithRedirect(auth, provider)
-}
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider);
+};
 
 const SignIn = () => {
   return (
     <div className={style.centrar}>
-        <GoogleButton onClick={googleSignIn} />
-    </div>
-  )
-}
+      <button
+        onClick={() => {
+          googleSignIn();
+        }}
+        className={style.button}
+      >
+        <img
+          width="40px"
+          src={"https://cdn-icons-png.flaticon.com/512/2111/2111450.png"}
+        />
+        {/* <h5 className={style.h5}>SignIn</h5> */}
+      </button>
 
-export default SignIn
+      {/* <GoogleButton onClick={googleSignIn} /> */}
+    </div>
+  );
+};
+
+export default SignIn;
